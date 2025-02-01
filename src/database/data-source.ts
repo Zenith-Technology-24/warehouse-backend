37d7030469d5565eeede421e3f08dcdc05c5e032
novidash <spacecat34@gmail.com>
2025-02-01 19:48:19 +0800

    Fix datasource

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,21 +1,35 @@
-import { DataSource } from 'typeorm';
-import { User } from '../models/user.entity';
-import 'dotenv/config';
-import { Inventory } from '../models/inventory.entity';
-import { Sales } from '../models/sales.entity';
-import { Customer } from '../models/customer.entity';
-import { SalesInventory } from '../models/sales_inventories.entity';
-import { Expense } from '../models/expense.entity';
-import { Role } from '../models/role.entity';
+import { DataSource, DataSourceOptions } from "typeorm";
+import { User } from "../models/user.entity";
+import "dotenv/config";
+import { Inventory } from "../models/inventory.entity";
+import { Sales } from "../models/sales.entity";
+import { Customer } from "../models/customer.entity";
+import { SalesInventory } from "../models/sales_inventories.entity";
+import { Expense } from "../models/expense.entity";
+import { Role } from "../models/role.entity";
+
+// @ts-expect-error malformed
+const options: DataSourceOptions =
+  process.env.NODE_ENV === "local"
+    ? {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DB,
+      }
+    : {
+        url: process.env.DB_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      };
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB,
-    entities: [User, Inventory, Sales, Customer, SalesInventory, Expense, Role],
-    synchronize: true,
-    logging: true,
+  // @ts-expect-error malformed
+  type: "postgres",
+  entities: [User, Inventory, Sales, Customer, SalesInventory, Expense, Role],
+  synchronize: true,
+  logging: true,
+  ...options,
 });
