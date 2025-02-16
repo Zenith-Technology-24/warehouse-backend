0ad5dcca6705115fbed1f50e6ccb3a7119c97a60
Elijah <elijah.abgao@fligno.com>
2025-02-16 16:08:55 +0800

    Fix strcture

--- src/services/end-user.service.ts
+++ src/services/end-user.service.ts
@@ -1,44 +1,45 @@
 import prisma from "@/generic/prisma";
 import { EndUser } from "@prisma/client";
 
-
 export class EndUserService {
-    async createEndUser(data: any) {
-        return await prisma.endUser.create({
-        data: {
-            ...data,
-        },
-        });
-    }
-    
-    async getEndUserById(id: string) {
-        return await prisma.endUser.findUnique({
-        where: {
-            id,
-        },
-        });
-    }
-    
-    async getEndUsers() {
-        return await prisma.endUser.findMany();
-    }
-    
-    async updateEndUser(id: string, data: any) {
-        return await prisma.endUser.update({
-        where: {
-            id,
-        },
-        data: {
-            ...data,
-        },
-        });
-    }
-    
-    async deleteEndUser(id: string) {
-        return await prisma.endUser.delete({
-        where: {
-            id,
-        },
-        });
-    }
+  async createEndUser(data: any) {
+    return await prisma.endUser.create({
+      data: {
+        ...data,
+      },
+    });
+  }
+
+  async getEndUserById(id: string) {
+    return await prisma.endUser.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async getEndUsers() {
+    return {
+        data: await prisma.endUser.findMany(),
+    };
+  }
+
+  async updateEndUser(id: string, data: any) {
+    return await prisma.endUser.update({
+      where: {
+        id,
+      },
+      data: {
+        ...data,
+      },
+    });
+  }
+
+  async deleteEndUser(id: string) {
+    return await prisma.endUser.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
